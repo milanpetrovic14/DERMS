5049cb128548d4551cde83171b7bdc2bcf69ec8f
filip32 <filipovic.filip32@gmail.com>
2020-01-28 23:38:28 +0100

    WeatherForecast - Dark Sky Api

--- DERMS/CalculationEngine/CalculationEngine/Program.cs
+++ DERMS/CalculationEngine/CalculationEngine/Program.cs
@@ -5,13 +5,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WeatherForecast;
 namespace CalculationEngine
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DarkSkyApi darkSkyApi = new DarkSkyApi();
+            darkSkyApi.GetWeatherForecastAsync(45.25167, 19.83694);
+
             ServiceManager serviceManager = new ServiceManager();
             ClientSideCE n = ClientSideCE.Instance;
 
